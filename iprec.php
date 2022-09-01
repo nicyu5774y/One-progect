49195c56aef8fb8afab4bb7bf463bf3f6c8d3337
nic <51706333+nicyu5774y@users.noreply.github.com>
2022-09-01 14:10:12 +0200

    Recorder Ip in to myslq server
    Sql+php(file)

---
+++ iprec.php
@@ -0,0 +1,64 @@
+
+Info: djpixeldj@gmail.com_addref
+WhatsApp: +1 (563) 281-2225 (IT,Italy)
+
+
+<?php
+$host = "you_host";
+$user = "root";
+$password = "password";
+$db = "database";
+
+$obj = new mysqli($host, $user, $password, $db);
+
+//INSER
+
+
+
+if (mysqli_connect_errno()) {
+   printf("Connect failed: %­s\n", mysqli_connect_error());
+   exit();
+}
+
+$ip = getenv("REMOTE_ADDR");
+$server = getenv("SERVER_NAME");
+
+$server2 = getenv("HTTP_HOST");
+$server3 = getenv("HTTP_REFERER");
+$server4 = getenv("HTTP_USER_AGENT");
+$server5 = getenv("SCRIPT_NAME");
+$server6 = getenv("GATEWAY_INTERFACE");
+$server7 = getenv("SERVER_SOFTWARE");
+$server8 = getenv("SERVER_PORT");
+$server9 = getenv("REQUEST_METHOD");
+$server10 = getenv("REQUEST_TIME");
+$server11 = getenv("QUERY_STRING");
+$server12 = getenv("HTTP_ACCEPT");
+$server13 = getenv("HTTP_ACCEPT_CHARSET");
+$server14 = getenv("HTTPS");
+$server15 = getenv("SERVER_ADMIN");
+$server16 = getenv("SERVER_SIGNATURE");
+$server17 = getenv("PATH_TRANSLATED");
+$server18 = getenv("SCRIPT_NAME");
+$server19 = getenv("SCRIPT_URI");
+$server20 = getenv("HTTP_headername");
+
+
+
+
+
+
+
+
+$SQL = sprintf("INSERT INTO ip (ip, server, server2, server3, server4, server5, server6, server7, server8, server9,server10,server11,server12,server13,server14,server15,server16,server17,server18,server19,server20)
+values('$ip','$server','$server2','$server3','$server4','$server5','$server6','$server7','$server8','$server9','$server10','$server11','$server12','$server13','$server14','$server15','$server16','$server17','$server18','$server19','$server20')");
+$result = $obj->query($SQL);
+
+if($result) {
+echo "IP Registrato...";
+} else {
+echo "Errore nella Query <-> Reset...";
+}
+/*                                        ^
+In caso non dovesse funzionare contact my |
+*/
---
+++ iprec.sql
@@ -0,0 +1,33 @@
+
+-- Struttura della tabella `ip`
+--
+
+CREATE TABLE `ip` (
+  `ip` varchar(20) DEFAULT NULL,
+  `server` varchar(35) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `data` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `server2` text NOT NULL,
+  `server3` text NOT NULL,
+  `server4` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `server5` text NOT NULL,
+  `server6` text NOT NULL,
+  `server7` text NOT NULL,
+  `server8` text NOT NULL,
+  `server9` text NOT NULL,
+  `server10` text NOT NULL,
+  `server11` text NOT NULL,
+  `server12` text NOT NULL,
+  `server13` text NOT NULL,
+  `server14` text NOT NULL,
+  `server15` text NOT NULL,
+  `server16` text NOT NULL,
+  `server17` text NOT NULL,
+  `server18` text NOT NULL,
+  `server19` text NOT NULL,
+  `server20` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

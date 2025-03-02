4e548b2d4772871ab81490f137ba99146721912d
NICK___________0_0...........'_'...................hahaha <51706333+nicyu5774y@users.noreply.github.com>
2025-03-02 12:43:49 +0100

    Update iprec.sql

--- iprec.sql
+++ iprec.sql
@@ -1,31 +1,28 @@
---
 -- Struttura della tabella `ip`
---
 CREATE TABLE `ip` (
-  `ip` varchar(20) DEFAULT NULL,
-  `server` varchar(35) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `data` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `server2` text NOT NULL,
-  `server3` text NOT NULL,
-  `server4` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `server5` text NOT NULL,
-  `server6` text NOT NULL,
-  `server7` text NOT NULL,
-  `server8` text NOT NULL,
-  `server9` text NOT NULL,
-  `server10` text NOT NULL,
-  `server11` text NOT NULL,
-  `server12` text NOT NULL,
-  `server13` text NOT NULL,
-  `server14` text NOT NULL,
-  `server15` text NOT NULL,
-  `server16` text NOT NULL,
-  `server17` text NOT NULL,
-  `server18` text NOT NULL,
-  `server19` text NOT NULL,
-  `server20` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+  `ip` VARCHAR(45) NOT NULL PRIMARY KEY,
+  `server` VARCHAR(35) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `data` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `server2` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `server3` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `server4` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `server5` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `server6` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `server7` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `server8` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `server9` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `server10` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `server11` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `server12` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `server13` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `server14` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `server15` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `server16` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `server17` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `server18` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `server19` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `server20` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-SITE: https://nas326-server.zyxel.me
